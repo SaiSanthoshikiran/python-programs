ce52c7d68e462e559cb18cb958243055e64315d8
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-17 20:44:35 +0530

    Add files via upload

---
+++ basic chat program.py
@@ -0,0 +1,16 @@
+i=input('enter your msg')
+if i in('Hi','em chestunnav','tinnava','inkenti','ha'):
+    if i=='Hi':
+        print(("hi"))
+    elif i=='em chestunnav':
+        print("kali")
+    elif i=='tinnava':
+        print("ha Nuvvu")
+    elif i=='inkenti':
+        print("cheppu")
+    elif i=='ha':
+        print('Ha')
+    else:
+        print('Gm/Gn')
+else:
+    print("empty text box")
