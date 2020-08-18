3b795eea37a0e322fc69c1ebdb40954cfd056f47
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-18 13:52:43 +0530

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,13 @@
+
+def add(x,y,z):
+     c=x+y+z
+     print(c)
+add(x,y,z)
+#--------------->>
+i=0
+for i in range(0,i<=100):
+    print(i)
+#----------------->>
+for i in range(1,11,):
+ i+=1
+ print(i)
