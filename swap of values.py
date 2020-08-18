cbdc54d72dad6f2bf8cf4e3ad6b313af0f39fc04
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-18 13:53:31 +0530

    Update swap of values.py

--- swap of values.py
+++ swap of values.py
@@ -1,22 +1,3 @@
-#a,b=map(int,input("enter").split('_'))
-#print(a+b)
-
-
-#n=int(input('enter'))
-#i=2
-#while i<=n:
-#   print(n,end="")
-#i+=1
-
-#n=int(input('enter'))
-#s=int(input('start'))
-#r=int(input('range'))
-#if s<n:
-#    for i in range (s,r+1,-1):
-#        print(n,'*',i,'=',n*i)
-#else:
-#    for i in range(r,s-1,-1):
-#        print(n,'*',i,'=',n*i)
 
 a=10
 b=20
