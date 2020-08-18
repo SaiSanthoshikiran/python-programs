df6a4f0274e328f588277d489b553052d6653c56
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-18 13:33:59 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -2,15 +2,7 @@
 #   print(i)
 #--------->
 
-#n=int(input('enter value'))
-#count=0
-#for i in range(1,n+1):
- #   if n%i==0:
-  #      count+=1
-#if count==2:
- #   print('prime num is',n)
-#else:
- #   print(n,'not prime')
+
 #------->
 n=int(input('enter'))
 if n%2==0:
