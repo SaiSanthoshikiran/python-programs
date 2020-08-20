5d862b9adb540a11ca939de23e85b2dedeb44508
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-20 12:26:00 +0530

    Add files via upload

---
+++ math module.py
@@ -0,0 +1,20 @@
+##import math
+##a=math.ceil(23.3567)
+##print(a)
+##a=math.floor(23.3567)
+##print(a)
+
+#------>>
+##from math import ceil#importing only ceil ,so only ceil sction is done 
+##
+##a=ceil(23.3567)
+##print(a)
+##b=floor(23.3567)
+##print(b)
+
+#------>>
+from math import *#*implies all actions are downloaded
+a=ceil(23.3567)
+print(a)
+b=floor(23.3567)
+print(b)
