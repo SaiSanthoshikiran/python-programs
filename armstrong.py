c13a8c8b9e2f9f0376c08c3eefd0896715cecaf9
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-20 18:11:27 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,16 +1,16 @@
 
 def sum_digits(n):
-    #n=l
+    n=l
     s=0
     while n>0:
         rem=n%10
         s+=rem**3
         n//=10
         return s
-##    if l==s:
-##        print('it is arm')
-##    else:
-##        print('not arm')
-##l=int(input('number'))
-##print(sum_digits(l))
+    if l==s:
+        print('it is arm')
+    else:
+        print('not arm')
+l=int(input('number'))
+print(sum_digits(l))
 
