48dbc8c9104f55ba1b1ec7072fbb5b9ac363c3aa
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-23 17:47:33 +0530

    Add files via upload

---
+++ alphabet repeatation.py
@@ -0,0 +1,13 @@
+n=int(input("enter the value"))
+for i in range (0,n+1):
+    for j in range  (i):
+        print("!",end="")
+    print()   
+
+##
+##n=int(input("number"))
+##
+##for i in range(1,n+1):
+##    for j in range(i):
+##        print("*",end="")
+##    print()
