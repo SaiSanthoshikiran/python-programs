2cb1fdf3a035c0358c2dfdc64147229f0e82691b
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-24 14:10:04 +0530

    Add files via upload

---
+++ consecutive num(list).py
@@ -0,0 +1,16 @@
+
+
+l = [2, 3, 8, 4, 5] 
+s=sorted(l)
+cons=s[0]<s[1]
+
+for i in range(len(l)):#
+    if cons:
+        s[i]-s[i-1]==1
+    else:
+        s[i]-s[i-1]!=1
+
+if cons:
+    print(s,"consecutive")
+else:
+    print(s,"not consecutive")
