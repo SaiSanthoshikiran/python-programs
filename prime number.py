5f91a16b89cf385c2a1414a119527a777c28ecd8
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-24 17:44:12 +0530

    Update prime number.py

--- prime number.py
+++ prime number.py
@@ -7,3 +7,15 @@ if count==2:
     print('prime num is',n)
 else:
     print(n,'not prime')
+##---------------->>
+#"to the list of prime num in a given range"
+import math
+s=int(input('enter  start value'))
+e=int(input('enter ending value'))
+for i in range (s,e+1):
+    for j in range (2,abs(int(math.sqrt(i))+1)):
+        if i%j==0:
+            break
+    else:
+        print(i,end=" ")
+            
