527f8bd49720bdc7804a7cca70f9e795542a79d4
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-26 18:23:58 +0530

    Add files via upload

---
+++ split.py
@@ -0,0 +1,10 @@
+##n=input('enter the value')
+##print(list(n.split(" ")))
+
+#---------------->>
+def split_string(string):
+    l=list(string.split("*"))
+    return l
+
+str=input("enter the input")
+print(split_string(str))
