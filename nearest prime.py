e484c0bb0850eb063e9bf5b61ec06ddd06c8d4ca
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-28 17:21:42 +0530

    Update nearest prime.py

--- nearest prime.py
+++ nearest prime.py
@@ -3,8 +3,8 @@ def prime(n):
      for i in range(2,abs(int(math.sqrt(n)))+1):
         if n%i==0:
             break
-        else:
-            return(1)
+     else:
+        return(1)
 n=int(input("enter a number"))
 for i in range(n-1,0,-1):
     if prime(i)==1:
@@ -24,4 +24,3 @@ elif abs(n-a)==abs(n-b):
     print(a,b,'both are near prime numbers')
 else:
     print(a,'is near prime number')
-###error is present
