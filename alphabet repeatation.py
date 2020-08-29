84d8897671e213cb94d5c0cc9fdf4bb17eb48d01
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-08-29 18:18:31 +0530

    Update alphabet repeatation.py

--- alphabet repeatation.py
+++ alphabet repeatation.py
@@ -2,7 +2,16 @@ n=int(input("enter the value"))
 for i in range (0,n+1):
     for j in range  (i):
         print("!",end="")
-    print()   
+    print() 
+ 
+[#output:
+#enter the value5
+!
+!!
+!!!
+!!!!
+!!!!!]
+------------->>
 
 ##
 ##n=int(input("number"))
@@ -17,3 +26,9 @@ for i in n:
     for j in range (len(n)):
         print(chr(i+65),end=" ")
     print()
+#OUTPUT:
+##K K K K K 
+## I I I I I 
+##  R R R R R 
+##A A A A A 
+##N N N N N 
