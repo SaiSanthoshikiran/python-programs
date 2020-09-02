33de427415230369db937c509aaf01936c17c6c4
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-02 16:38:34 +0530

    Update alphabet repeatation.py

--- alphabet repeatation.py
+++ alphabet repeatation.py
@@ -13,13 +13,12 @@ for i in range (0,n+1):
 !!!!!]
 ------------->>
 
-##
-##n=int(input("number"))
-##
-##for i in range(1,n+1):
-##    for j in range(i):
-##        print("*",end="")
-##    print()
+n=int(input("number"))
+
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end="")
+    print()
 #----------->>
 n=[10,8,17,0,13]
 for i in n:
