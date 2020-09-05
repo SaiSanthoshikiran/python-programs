be9d703800d529fe8c305ea50d9ecae86faccb78
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-05 20:20:49 +0530

    Update Multiplication Table.py

--- Multiplication Table.py
+++ Multiplication Table.py
@@ -1,7 +1,7 @@
 
-n=int(input('enter'))
-s=int(input('start'))
-r=int(input('range'))
+n=int(input('enter the number'))
+s=int(input(' enter starting num'))
+r=int(input('enter range'))
 if s<n:
     for i in range (s,r+1,-1):
         print(n,'*',i,'=',n*i)
