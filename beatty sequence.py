781e668c82ae72f2911717e307736f83b2c0e2e6
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-08 17:23:49 +0530

    Add files via upload

---
+++ beatty sequence.py
@@ -0,0 +1,9 @@
+import math
+def beatty(n):
+    for i in range(1,n+1):
+        a=int(i*(2**0.5))
+        #for above line we can write as below one
+        #a=math.floor(i*math.sqrt(2))
+        print(a)
+n=int(input("n="))
+beatty(n)
