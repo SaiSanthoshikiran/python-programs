634123e1c40087fcff470dde313ab51dde01ac1a
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-10 20:23:59 +0530

    Add files via upload

---
+++ happy num.py
@@ -0,0 +1,21 @@
+def num(n):
+    s=0
+    while(n>0):
+        r=n%10
+        s=s+r**2
+        n=n//10
+    return(s)
+def happy(n):
+    while n>10:
+        k=num(n)
+        if k==1 or k==7:
+            return('yes')
+        else:
+            n=k
+    else:
+        if n==1 or n==7:
+            return('yes')
+        else:
+            return('no')
+n=int(input("n="))
+print(happy(n))
