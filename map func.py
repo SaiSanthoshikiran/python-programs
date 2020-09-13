c116f4b3eba88cf7c8c64bdf3a6c2a6bfdc9bd2b
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-13 21:53:28 +0530

    Update map func.py

--- map func.py
+++ map func.py
@@ -2,3 +2,10 @@ def m(a,b):
     return(a+b)
 n=map(m,('ravindra','sri','sai','jaswanth'),('kumar','devi','santhoshikiran','kante'))
 print(list(n))
+
+----------------------->>
+
+def m(n):
+    return(len(n))
+n=map(m,('kiran','santoshi'))
+print(list(n))
