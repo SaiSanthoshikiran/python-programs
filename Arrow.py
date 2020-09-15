78c368ad297cc0a36b1cbcbff3567881a8b60599
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-15 22:19:05 +0530

    Add files via upload

---
+++ Arrow.py
@@ -0,0 +1,9 @@
+def pattern(n):
+    for i in range(n):#2,-2
+        for j in range(1,abs(i)+1):
+            print(' ',end="")
+        for k in range(1,abs(i)+2):
+            print('* ',end="")
+        print()
+n=int(input('n'))
+pattern(n)
