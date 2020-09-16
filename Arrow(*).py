7a30b4239054f370c72cf0d9c2736efe91c31995
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-16 14:39:30 +0530

    Update Arrow(*).py

--- Arrow(*).py
+++ Arrow(*).py
@@ -16,3 +16,20 @@ def pattern(n):
 n=int(input('n'))
 pattern(n)
 
+----------------------->>
+
+def pattern(n):
+    for i in range(1,n):#1,2
+        for j in range(n-i,0,-1):#3,2
+            print(' ',end="")
+        for k in range(n-i):
+            print('*',end="")
+        print()
+    for i in range(0,n):
+        for j in range(0,i):#3,2
+            print(' ',end="")
+        for k in range(i):
+            print('*',end="")
+        print()
+n=int(input('n'))
+pattern(n)
