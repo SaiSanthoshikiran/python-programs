919c4f0d8a23e98529115107fad75a7f40c81261
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-20 22:12:07 +0530

    Update happy num.py

--- happy num.py
+++ happy num.py
@@ -19,3 +19,23 @@ def happy(n):
             return('no')
 n=int(input("n="))
 print(happy(n))
+
+
+-------------------->>
+
+n=int(input())
+res=0
+while True:
+    d=n%10
+    res=res+d*d
+    n=n//10
+    
+    if res<9 and n==0:
+        break
+    if n==0:
+        n=res
+        res=0
+if res==1 or res==7:
+    print('happy')
+else:
+    print('not happy')
