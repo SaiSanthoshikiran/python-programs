4891759f85d464b9e2d39a5742d3010122bba9ff
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-22 20:56:47 +0530

    Update Octal-Decimal.py

--- Octal-Decimal.py
+++ Octal-Decimal.py
@@ -1,3 +1,4 @@
+octal-->>decimal
 ##n=int(input('n:'))
 ##decimal=0
 ##b=1
@@ -10,12 +11,43 @@
 
 #---------->>
 
+#O -->> D
+n=int(input('n'))
+c=0
+for i in range (0,len(str(n))):
+    rem=n%10
+    c+=(rem*8**i)
+    n=n//10
+print(c)
+------------------>>
+
 #Decimal ---->> Octal
 
 n=int(input('n:'))
 b=''
 while n:
-    b+=str(n%2)
-    n=n//2
-    
+    b+=str(n%8)
+    n=n//8
 print(b[::-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
