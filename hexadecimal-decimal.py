5b819e3892a1a6ed7cb30f5c4a0a0ee32b64ef2b
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-23 17:41:07 +0530

    Add files via upload

--- hexadecimal-decimal.py
+++ hexadecimal-decimal.py
@@ -1,4 +1,4 @@
-#Binary -->> Decimal
+#Decimal---->>Hexa Decimal
 n=int(input('h'))
 h=''
 while n:
@@ -8,3 +8,15 @@ while n:
     else:
         h+=str(chr(r+55))
 print(h[::-1])
+
+#hexa-->>Decimal
+n=int(input('n'))
+d=0
+b=1
+for i in ra nge(len()):
+    r=n[-1]
+    if r.isdigit():
+        d+=(ord()-48)*b
+    else:
+        d+=(ord(r)-55)
+    n=n[:-1 ]
