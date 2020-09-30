6c2468aef3c638a0dcfee98c559ec459f2becef6
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-30 17:06:11 +0530

    Update num in right ange shape.py

--- num in right ange shape.py
+++ num in right ange shape.py
@@ -13,3 +13,20 @@ num:4
 1 2 3 4
 
 ------------------------>>
+
+num=int(input('num:'))
+
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print(i,end=' ')
+    print()
+
+OUTPIT:
+num:4
+1 
+2 2 
+3 3 3 
+4 4 4 4
+
+------------------>>
+
