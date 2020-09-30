28cb918d0d317c40fedabbd070c925ed9b9ab64e
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-09-30 17:04:04 +0530

    Add files via upload

---
+++ num in right ange shape.py
@@ -0,0 +1,21 @@
+##num=int(input('num:'))
+
+##for i in range(1,num+1):
+##    for j in range(1,i+1):
+##        print(i,end=' ')
+##    print()
+##
+##OUTPIT:
+##num:4
+##1 
+##2 2 
+##3 3 3 
+##4 4 4 4
+##
+##------------------>>
+num=int(input('num:'))
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+
