633f47e9f1d2ae3f8d11f91d24fd081c806ada6d
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-10-06 09:06:25 +0530

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -19,5 +19,30 @@ print(a)
 
 ##OUTPUT:
 ##[1, 2, 3, 3, 2, 0, 0, 0]
-##
-##
+
+====================================>>
+n=[1,2,3,4,5]
+#for i in n:
+ # print(i)
+#------------->>
+for i in range(1,5):
+  print(i)
+#------------>>
+n="kiran"
+for i in range (len(n)):
+ print(n[i],end="  ")
+print()
+#--------->>
+n="kiran"
+for i in n:
+ print(i,end=" ")
+
+
+
+
+
+
+
+
+
+
