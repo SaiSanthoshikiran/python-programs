f18c8357ae92b564753d9e8829df89fbe6f17f8c
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-10-06 08:37:51 +0530

    Add files via upload

---
+++ combing lists.py
@@ -0,0 +1,18 @@
+##a=[1,2,3,4]
+##b=[2,5,6]
+##for i in b:
+##    if i in a:
+##        pass
+##    else:
+##        a.append(i)
+##print(a)
+##
+##------------------>>
+
+a=[1,2,3,4]
+b=[2,5,6]
+for i in b:
+    if i not in a:
+        a.append(i)
+    
+print(a)    
