ce3850b5f0e8517d5cc1382bd48f5b60958a92c3
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-10-19 09:17:41 +0530

    Update diction sum.py

--- diction sum.py
+++ diction sum.py
@@ -1,22 +1,20 @@
 d={}
+d2={}
 n=int(input('no.of.students:'))
 s=int(input('no.of subjects:'))
 l=[]
 for i in range(n):
     name=input('name:')
     m=list(map(int,input('marks:').split()))
+    d[name]=m
     c=0
     for j in m:
         c=c+j
-    l.append(c)
-print(l)
-        
-        
-        
-        
-    
-    
-print(d)
-        
-        
+        l.append(c)
+        d2[name]=l
+m=max(l)
+
+print("Intial list:",d)
+print('max value:',d2)
+
         
