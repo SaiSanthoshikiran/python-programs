ab3543767ac67bbaad6e00ac9831b134524fabc2
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-10-19 08:54:38 +0530

    Add files via upload

---
+++ diction sum.py
@@ -0,0 +1,22 @@
+d={}
+n=int(input('no.of.students:'))
+s=int(input('no.of subjects:'))
+l=[]
+for i in range(n):
+    name=input('name:')
+    m=list(map(int,input('marks:').split()))
+    c=0
+    for j in m:
+        c=c+j
+    l.append(c)
+print(l)
+        
+        
+        
+        
+    
+    
+print(d)
+        
+        
+        
