416c8813d17c2651eb561adaba5324ad530f0001
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-10-27 21:17:22 +0530

    Add files via upload

---
+++ target.py
@@ -0,0 +1,13 @@
+def fun(fb,n):
+    fb=[0]+fb+[0]
+    c=0
+    for i in range(len(fb)):
+        if fb[i:i+3]==[0,0,0]:
+            c+=1
+            if c>=n:
+                return True
+            fb[i+1]=1
+    return c>=n
+fb=[1,0,0,0,1]
+n=1
+print(fun(fb,n))
