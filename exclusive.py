29570b831b68be0ae3c09298f843f8d0c451962d
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-10-29 09:15:45 +0530

    Update exclusive.py

--- exclusive.py
+++ exclusive.py
@@ -1,4 +1,9 @@
+#"printing sum of arr by excluding last and first elements "
 def exclusive(n):
-    return(sum(n[1:-1])//(len(n)-2))
+    return(sum(n[1:-1])/(len(n)-2))
 n=list(map(int,input().split()))
 print(exclusive(n))
+
+OUTPUT:
+1 2 3 4 5
+3.0
