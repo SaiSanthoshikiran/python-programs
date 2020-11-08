e783528abc5419a1300f010b0df219dcb69de94d
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-11-08 19:00:18 +0530

    Update simple interest.py

--- simple interest.py
+++ simple interest.py
@@ -1,5 +1,13 @@
 p=int(input("enter principle amount"))
 t=int(input("t="))
 r=int(input("r="))
-ans=(p*t/100*r)
+ans=(p*t*r)/100
 print(ans)
+
+#compound intrest
+p=int(input("enter principle amount"))
+t=int(input("t="))
+r=int(input("r="))
+n=int(input('n='))
+a=(p*(1+r/n))**0.5
+print(a)
