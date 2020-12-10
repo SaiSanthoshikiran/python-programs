a2b470dc00647e49b3b79e36e11be2d86c72c0e1
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-12-10 20:57:08 +0530

    Add files via upload

---
+++ subarray sum.py
@@ -0,0 +1,15 @@
+n=[4,-2,-1,1,-2,3,-3]
+k=2
+c=0
+##for i in range(len(n)):
+##    x=0
+##    for j in range(i,len(n)):
+##        x=sum[i:j+1]
+##        if x==k:
+##            c+=1
+##    print(c)
+##        
+for i in range(len(n)):
+    c=c+n[i]
+    print(c)
+        
