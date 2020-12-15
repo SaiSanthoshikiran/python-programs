cbf7ccbf819f394de1f2d82763e5a2c645a2164a
Sai Santhoshi Kiran <62435650+SaiSanthoshikiran@users.noreply.github.com>
2020-12-15 20:58:26 +0530

    Add files via upload

---
+++ selection.py
@@ -0,0 +1,11 @@
+clg=input()
+yr=int(input())
+per=int(input())
+backlog=int(input())
+brnch=input()
+gender=input()
+if not(brnch=="cse" or brnch=="ece" or brnch=="it" and clg=="aec" and yr=="4" and per>="60" and backlog=="0" and gender=="f"):
+    print("you are not eligible")
+else:
+    print("you are not eligible")
+ 
